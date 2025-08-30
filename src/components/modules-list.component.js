2ea1c711925b1d0c8cf39f5b80ad5db52d11a471
kalyan-ismart <rathlavathkalyan6@gmail.com>
2025-08-30 12:23:17 +0530

    Fix: change delete link to button for deployment

--- src/components/modules-list.component.js
+++ src/components/modules-list.component.js
@@ -9,7 +9,7 @@ const Module = props => (
     <td>{props.module.duration}</td>
     <td>{props.module.date.substring(0,10)}</td>
     <td>
-  <Link to={"/edit/"+props.module._id}>edit</Link> | <button onClick={() => { props.deleteModule(props.module._id) }} className="btn btn-link p-0">delete</button>
+      <Link to={"/edit/"+props.module._id}>edit</Link> | <button onClick={() => { props.deleteModule(props.module._id) }} className="btn btn-link p-0">delete</button>
     </td>
   </tr>
 )
@@ -17,14 +17,11 @@ const Module = props => (
 export default class ModulesList extends Component {
   constructor(props) {
     super(props);
-
     this.deleteModule = this.deleteModule.bind(this);
-
     this.state = {modules: []};
   }
 
   componentDidMount() {
-    // This now points to your live Render server
     axios.get('https://ssc-prep-suite-backend-123.onrender.com/modules/')
       .then(response => {
         this.setState({ modules: response.data })
@@ -35,10 +32,9 @@ export default class ModulesList extends Component {
   }
 
   deleteModule(id) {
-    // This now points to your live Render server
     axios.delete('https://ssc-prep-suite-backend-123.onrender.com/modules/'+id)
       .then(res => console.log(res.data));
-    
+
     this.setState({
       modules: this.state.modules.filter(el => el._id !== id)
     })
